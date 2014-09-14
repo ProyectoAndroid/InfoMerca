dd85dff72c5487e622bd103ecc4d81ea1b544a85
Ghondar <abgv9221@gmail.com>
2014-09-14 00:23:25 -0500

    Agregado categorias y subcategorias

--- linio.py
+++ linio.py
@@ -1,16 +1,31 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-url = "http://www.linio.com.pe/tv-audio-y-video/televisores/televisores/?sort=price&dir=asc"
+cantidad = 1
 
-content = urllib2.urlopen(url).read()
+linkPrincipal = "http://www.linio.com.pe/"
 
-soup = BeautifulSoup(content)
-for paquete in soup.find_all("ul", id="catalog-items"):
-    #print nombre_producto.string.lstrip()
-    #if nombre_producto.get('title') is not None:
-    #    print nombre_producto.get('title')
-    resultado = BeautifulSoup(str(paquete))
+categorias = ("tv-audio-y-video", "computadoras", "camaras", "videojuegos", "celulares-telefonia-y-gps")
 
-    for nombre_resultado in resultado.find_all("a"):
-        print nombre_resultado['title']
+subcategorias = [["televisores", "accesorios-para-tv", "reproductores-de-video", "cine-en-casa"],
+                 ["laptops-y-notebooks", "desktops", "apple", "tablets-y-accesorios", "gamers"],
+                 ["digitales", "profesionales", "videocamaras"], ["preventas", "playstation", "nintendo", "xbox-360"],
+                 ["celulares-iphones", "celulares", "accesorios-para-celulares", "gps"]]
+
+for categoria in range(categorias.__len__()):
+
+    for subcategoria in range(subcategorias[categoria].__len__()):
+
+        for numeroPagina in range(cantidad):
+
+            url = linkPrincipal + categorias[categoria] + "/" + subcategorias[categoria][subcategoria] + "/?page=" + str(numeroPagina)
+
+            content = urllib2.urlopen(url).read()
+
+            soup = BeautifulSoup(content)
+            for paquete in soup.find_all("ul", id="catalog-items"):
+
+                resultado = BeautifulSoup(str(paquete))
+
+                for nombre_resultado in resultado.find_all("a"):
+                    print nombre_resultado['title']
